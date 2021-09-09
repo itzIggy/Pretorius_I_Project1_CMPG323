10574b4e3aaaa04aff39f2c27956b7d75d626b58
itzIggy <ivan7q20@gmail.com>
2021-09-09 19:17:53 +0200

    add

--- AcSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountType.java
+++ AcSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountType.java
@@ -4,8 +4,23 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
+import java.util.Set;
 
 //@Entity
 //@Table(name = "ACCOUNT_TYPE", schema = "DISCOVERY")
-public class AccountType {
+public class AccountType implements Serializable{
+
+        private Long accountType_ID;
+        private String mnemonic;
+        private String accountType_Name;
+        private LocalDate dateCreated;
+        private Set<AccountTransaction> accountTransactionSet;
+
+        //@Contract(pure = true)
+        public AccountType(long accountType_ID, String mnemonic, String accountType_Name, LocalDate dateCreated){
+            this.accountType_ID = accountType_ID;
+            this.mnemonic = mnemonic;
+            this.accountType_Name = accountType_Name;
+            this.dateCreated = dateCreated;
+        }
 }
