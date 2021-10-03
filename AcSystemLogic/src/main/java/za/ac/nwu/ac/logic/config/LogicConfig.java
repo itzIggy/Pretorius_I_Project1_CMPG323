ead813c7b2a2e56b7784cc404673b46edd0ea1c5
itzIggy <ivan7q20@gmail.com>
2021-10-03 18:49:28 +0200

    Test Commit

--- AcSystemLogic/src/main/java/za/ac/nwu/ac/logic/config/LogicConfig.java
+++ AcSystemLogic/src/main/java/za/ac/nwu/ac/logic/config/LogicConfig.java
@@ -4,8 +4,9 @@ package za.ac.nwu.ac.logic.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import za.ac.nwu.ac.Translator.config.TranslatorConfig;
 
-//@Import({TranslatorConfig.class})
+@Import({TranslatorConfig.class})
 @Configuration
 @ComponentScan(basePackages = {
         "za.ac.nwu.ac.logic.flow"
--- AcSystemRepository/src/main/java/za/ac/nwu/ac/repo/persistence/AccountTypeRepository.java
+++ AcSystemRepository/src/main/java/za/ac/nwu/ac/repo/persistence/AccountTypeRepository.java
@@ -2,9 +2,9 @@ package za.ac.nwu.ac.repo.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import za.ac.nwu.ac.domain.persistence.AccountType;
 
-//extends JpaRepository<AccountType, Long>
 
 @Repository
-public interface AccountTypeRepository{
+public interface AccountTypeRepository extends JpaRepository<AccountType, Long>{
 }
